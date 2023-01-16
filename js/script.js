7b03ceeda98f4c105a8b07f73606a56f4fdd1e39
Miguel colmenarez <colmenarezmiguel24@gmail.com>
2023-01-16 17:11:12 -0400

    I add style and write more information about me

--- js/script.js
+++ js/script.js
@@ -1,48 +1,12 @@
-const Name = document.querySelector('[name=username]');
-const email = document.querySelector('[name=email]');
-const button = document.querySelector('[name=button]')
-const form = document.querySelector('[name=form]')
+const menu = document.getElementById('menu')
 
-const setErrors = (message, field, isError = true) => {
-    if(isError){
-        field.classList.add("invalid")
-        field.nextElementSibling.classList.add("error")
-        field.nextElementSibling.innerText = message;
+menu.addEventListener('scroll', (e) => {
+    const value = e.target.value
+    if(value == true) {
+        menu.classList.add('menu--active')
+        menu.classList.remove('menu')
     } else {
-        field.classList.remove('invalid')
-        field.nextElementSibling.classList.remove('error')
-        field.nextElementSibling.innerText = ""
+        menu.classList.remove('menu--active')
+        menu.classList.add('menu')
     }
-}
-
-
-const emptyField = (message, e) => {
-    const field = e.target
-    const value = e.target.value;
-    if (value.length === 0) {
-        setErrors(message, field)
-    }else {
-        setErrors("", field, false)
-    }
-}
-
-
-const validateEmail = e =>{
-    const field = e.target;
-    const data = e.target.value;
-    const regEx = new RegExp (/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/);
-    if(data.trim().length > 5 && !regEx.test(data)){
-        setErrors("Please enter a valid email", field)
-    } else {
-        setErrors("", field, false)  
-    }
-}
-
-
-Name.addEventListener('blur', (e) => emptyField("Name is required", e));
-email.addEventListener('blur', (e) => emptyField("Please enter a valid email", e));
-email.addEventListener('input', validateEmail);
-
-button.addEventListener('click',  (e) => {
-    form.submit();  
 })
